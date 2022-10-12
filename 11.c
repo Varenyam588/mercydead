1c9bc71b61a7c0354b320e3770de8fcbd2aba6a2
Varenyam588 <115102335+Varenyam588@users.noreply.github.com>
2022-10-12 10:49:32 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
-#include<ctype.h> // to use system defined function islower & toupper
 
 int main()
 {
     printf("RA2211042010061")
-
-int main()
-{
-    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
 
     char alphabet;
     printf("Enter an alphabet : ");
