8584eda98a4be5c321a0c324a3a49d2ad045903a
Varenyam588 <115102335+Varenyam588@users.noreply.github.com>
2022-11-24 01:57:20 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int H,A;
+    printf("Enter H,A : ");
+    scanf("%d %d",&H,&A);
+    int c=H/A;
+    if(H%A!=0)
+    {
+       c++;
+    }
+    printf("Number of attack needed = %d\n",c);
+}
