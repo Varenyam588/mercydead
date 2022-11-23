ed634064d151937a4875a6e1a37bd99ec65104f2
Varenyam588 <115102335+Varenyam588@users.noreply.github.com>
2022-11-24 02:45:30 +0530

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int checkPrimeNumber(int n);
+
+int main() {
+
+  int n, flag;
+
+  printf("Enter a positive integer: ");
+  scanf("%d",&n);
+
+  // n is passed to the checkPrimeNumber() function
+  // the returned value is assigned to the flag variable
+  flag = checkPrimeNumber(n);
+
+  if(flag == 1)
+    printf("%d is not a prime number",n);
+  else
+    printf("%d is a prime number",n);
+
+  return 0;
+}
+
+// int is returned from the function
+int checkPrimeNumber(int n) {
+
+  // 0 and 1 are not prime numbers    
+  if (n == 0 || n == 1)
+    return 1;
